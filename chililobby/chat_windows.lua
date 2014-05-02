181d361495ad713159dac965d64cf9a1cd48bb17
Gajo Petrovic <gajop@uns.ac.rs>
2014-05-02 08:53:54 +0200

    updated chililobby to use a single listener for each event

--- chililobby/chat_windows.lua
+++ chililobby/chat_windows.lua
@@ -1,18 +1,86 @@
 ChatWindows = LCS.class{}
 
 function ChatWindows:init()
-    local debugConsole = Console()
-    local lobbyListener = LobbyListener(self, debugConsole)
-    lobby:AddListener(lobbyListener)
-    debugConsole.listener = function(message)
+    -- setup debug console to listen to commands
+    self.debugConsole = Console()
+    self.debugConsole.listener = function(message)
         lobby:SendCustomCommand(message)
     end
+    lobby:Register("OnCommandReceived",
+        function(listner, command)
+            self.debugConsole:AddMessage("<" .. command)
+        end
+    )
+    lobby:Register("OnCommandSent",
+        function(listner, command)
+            self.debugConsole:AddMessage(">" .. command)
+        end
+    )
 
-    lobby:Channels()
+    -- get a list of channels when login is done
+    lobby:Register("OnLoginInfoEnd",
+        function(listener)
+            lobby:Unregister("OnLoginInfoEnd", listener)
+
+            self.channels = {} -- list of known channels retrieved from OnChannel
+            local onChannel = function(listener, chanName, userCount, topic)
+                self.channels[chanName] = { userCount = userCount, topic = topic }
+            end
+
+            lobby:Register("OnChannel", onChannel)
+            
+            lobby:Register("OnEndOfChannels",
+                function(listener)
+                    lobby:Unregister("OnEndOfChannels", listener)
+                    lobby:Unregister("OnChannel", onChannel)
+
+                    local channelsArray = {}
+                    for chanName, v in pairs(self.channels) do
+                        table.insert(channelsArray, { 
+                            chanName = chanName, 
+                            userCount = v.userCount, 
+                            topic = v.topic,
+                        })
+                    end
+                    table.sort(channelsArray, 
+                        function(a, b)
+                            return a.userCount > b.userCount
+                        end
+                    )
+                    self:UpdateChannels(channelsArray)
+                end
+            )
+
+            lobby:Channels()
+        end
+    )
+
+    self.channelConsoles = {}
+    lobby:Register("OnJoinReceived", 
+        function(listener, chanName)
+            local channelConsole = Console()
+            self.channelConsoles[chanName] = channelConsole
+
+            channelConsole.listener = function(message)
+                lobby:Say(chanName, message)
+            end
+
+            self.tabPanel:AddTab({name = "#" .. chanName, children = {channelConsole.panel}})
+        end
+    )
+
+    lobby:Register("OnSaid", 
+        function(listener, chanName, userName, message)
+            local channelConsole = self.channelConsoles[chanName]
+            if channelConsole ~= nil then
+                channelConsole:AddMessage(userName .. ": " .. message)
+            end
+        end
+    )
 
     self.serverPanel = ScrollPanel:New {
         x = 0,
-        width = "100%",
+        right = 5,
         y = 0,
         height = "100%",
     }
@@ -25,7 +93,7 @@ function ChatWindows:init()
         padding = {0, 0, 0, 0},
         tabs = {
             { name = "server", children = {self.serverPanel} },
-            { name = "debug", children = {debugConsole.panel} },
+            { name = "debug", children = {self.debugConsole.panel} },
         },
     }
 
@@ -42,4 +110,72 @@ function ChatWindows:init()
             self.tabPanel,
         }
     }
+end
+
+function ChatWindows:UpdateChannels(channelsArray)
+    self.serverPanel:ClearChildren()
+
+    self.serverPanel:AddChild(
+        Label:New {
+            x = 0,
+            width = 100,
+            y = 0,
+            height = 20,
+            caption = "#",
+        }
+    )
+    self.serverPanel:AddChild(
+        Label:New {
+            x = 50,
+            width = 100,
+            y = 0,
+            height = 20,
+            caption = "Channel",
+        }
+    )
+    self.serverPanel:AddChild(
+        Label:New {
+            x = 130,
+            width = 100,
+            y = 0,
+            height = 20,
+            caption = "Topic",
+        }
+    )
+    for i, channel in pairs(channelsArray) do
+        self.serverPanel:AddChild(Control:New {
+            x = 0,
+            width = "100%",
+            y = i * 50,
+            height = 40,
+            children = {
+                Label:New {
+                    x = 0,
+                    width = 100,
+                    y = 0,
+                    height = 20,
+                    caption = channel.userCount,
+                },
+                Label:New {
+                    x = 50,
+                    width = 100,
+                    y = 0,
+                    height = 20,
+                    caption = channel.chanName,
+                },
+                Button:New {
+                    x = 130,
+                    width = 60,
+                    y = 0,
+                    height = 30,
+                    caption = "Join",
+                    OnClick = {
+                        function()
+                            lobby:Join(channel.chanName)
+                        end
+                    },
+                },
+            }
+        })
+    end
 end
--- chililobby/login_window.lua
+++ chililobby/login_window.lua
@@ -62,6 +62,17 @@ function LoginWindow:init()
         },
     }
 
+    self.lblError = Label:New {
+        x = 1,
+        width = 100,
+        y = 100,
+        height = 80,
+        caption = "",
+        font = {
+            color = { 1, 0, 0, 1 },
+        },
+    }
+
     self.window = Window:New {
         x = 700,
         y = 300,
@@ -75,6 +86,7 @@ function LoginWindow:init()
             self.lblPassword,
             self.ebUsername,
             self.ebPassword,
+            self.lblError,
             self.btnLogin
         },
         parent = screen0,
@@ -82,17 +94,121 @@ function LoginWindow:init()
 end
 
 function LoginWindow:tryLogin()
+    self.lblError:SetCaption("")
+
     username = self.ebUsername.text
     password = self.ebPassword.text
     if username == '' or password == '' then
         return
     end
+
     lobby:Initialize()
 
-    lobby:Connect("springrts.com", "8200")
-    lobby:Login(username, password, 3)
+    lobby:Register("OnConnect", 
+        function(listener)
+            lobby:Unregister("OnConnect", listener)
 
-    local playWindow = PlayWindow()
-    local chatWindows = ChatWindows()
-    self.window:Dispose()
+            local onDenied = function(listener, reason)
+                self.lblError:SetCaption(reason)
+            end
+
+            lobby:Register("OnAccepted",
+                function(listener)
+                    local playWindow = PlayWindow()
+                    local chatWindows = ChatWindows()
+                    self.window:Dispose()
+                    lobby:Unregister("OnAccepted", listener)
+                    lobby:Unregister("OnDenied", onDenied)
+                end
+            )
+
+            lobby:Register("OnDenied", onDenied)
+            
+            local onAgreement = function(listener, line)
+                if self.agreementText == nil then
+                    self.agreementText = ""
+                end
+                self.agreementText = self.agreementText .. line .. "\n"
+            end
+            lobby:Register("OnAgreement", onAgreement)
+
+            lobby:Register("OnAgreementEnd", 
+                function(listener)
+                    self:createAgreementWindow()
+                    lobby:Unregister("OnAgreementEnd", listener)
+                    lobby:Unregister("OnAgreement", onAgreement)
+                end
+            )
+
+            lobby:Login(username, password, 3)
+        end
+    )
+
+    lobby:Connect("springrts.com", "8200")
+end
+
+function LoginWindow:createAgreementWindow()
+    self.tbAgreement = TextBox:New {
+        x = 1,
+        right = 1,
+        y = 1,
+        height = "100%",
+        text = self.agreementText,
+    }
+    self.btnYes = Button:New {
+        x = 1,
+        width = 80,
+        bottom = 1,
+        height = 40,
+        caption = "Accept",
+        OnClick = {
+            function()
+                self:acceptAgreement()
+            end
+        },
+    }
+    self.btnNo = Button:New {
+        x = 150,
+        width = 80,
+        bottom = 1,
+        height = 40,
+        caption = "Decline",
+        OnClick = {
+            function()
+                self:declineAgreement()
+            end
+        },
+    }
+    self.agreementWindow = Window:New {
+        x = 600,
+        y = 400,
+        width = 350,
+        height = 450,
+        caption = "Use agreement",
+        resizable = false,
+        children = {
+            ScrollPanel:New {
+                x = 1,
+                right = 7,
+                y = 1,
+                bottom = 42,
+                children = {
+                    self.tbAgreement
+                },
+            },
+            self.btnYes,
+            self.btnNo,
+
+        },
+        parent = screen0,
+    }
+end
+
+function LoginWindow:acceptAgreement()
+    lobby:ConfirmAgreement()
+    self.agreementWindow:Dispose()
+end
+
+function LoginWindow:declineAgreement()
+    self.agreementWindow:Dispose()
 end
