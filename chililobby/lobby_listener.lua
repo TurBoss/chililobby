3fd4e112ec9411226986e0c5a9819d40d90f4938
Gajo Petrovic <gajop@uns.ac.rs>
2014-05-03 19:31:40 +0200

    removed lobby_listener.lua

--- chililobby/lobby_listener.lua
+++
@@ -1,8 +0,0 @@
-LobbyListener = LibLobby.Listener:extends{}
-
-function LobbyListener:init(chatWindows, debugConsole)
-    self.chatWindows = chatWindows
-    self.debugConsole = debugConsole
-
-end
-
